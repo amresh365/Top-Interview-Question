4bfa96192b132f6491ad9ccf292fa86054bbc00a
Amresh Gupta <114059788+amresh365@users.noreply.github.com>
2024-05-29 10:41:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (72.62%) - LeetSync

---
+++ Array-String/27-remove-element/remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+           int i=0;
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]!=val)
+            {
+             int temp=nums[j];
+             nums[i]=temp;
+             i++;
+            }
+        }
+
+        return i;
+    }
+}
